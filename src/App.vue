522cd0c9eedfd14f1af1ba009f981c36dffa8fd4
carlo.bongiovanni@gmail.com <carlo.bongiovanni@gmail.com>
2023-05-24 18:24:28 +0200

    refactor pagination

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import SpaHeader from './components/SpaHeader.vue'
-import PageDescription from './components/PageDescription.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import SpaHeader from './components/SpaHeader.vue';
+import PageDescription from './components/PageDescription.vue';
 </script>
 
 <template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,25 +9,58 @@
     </ul>
   </div>
   <div>
-    <router-link :to="{ name: 'paginatedHomeView', params: { skip: 10 } }">next</router-link>
+    <paginate
+        id="unit"
+        :page-count="(parseInt((pageCounter / 10) + 1))"
+        :page-range="3"
+        :initial-page="0"
+        :margin-pages="0"
+        :click-handler="paginateCallback"
+        :first-last-button="true"
+        :hide-prev-next="true"
+        :prev-class="'ignore'"
+        :next-class="'ignore'"
+        :disabled-class="'ignore'"
+      ></paginate>
   </div>
 </template>
 
 <script>
+import Paginate from "vuejs-paginate-next";
 
 export default {
   data() {
     return {
       listItems: [],
+      page: 0,
+      pageCounter: 0
     }
+  },
+  components: {
+    paginate: Paginate,
   },
   methods: {
     async getItems() {
-      const FULL_URL = import.meta.env.VITE_API_URL + "/endpoint/events"
+      const FULL_URL = import.meta.env.VITE_API_URL + "/endpoint/paginatedEvents?skip=" + (this.page * 10);
       const res = await fetch(FULL_URL);
       const finalRes = await res.json();
       this.listItems = finalRes;
-    }
+      if (finalRes[0].totalCount) {
+        this.pageCounter = finalRes[0].totalCount
+      }
+    },
+    paginateCallback(pageNum) {
+      this.getPaginatedItems(pageNum)
+    },
+    async getPaginatedItems(pageNum) {
+      const FULL_URL = import.meta.env.VITE_API_URL + "/endpoint/paginatedEvents?skip=" + ((pageNum - 1) * 10);
+      const res = await fetch(FULL_URL);
+      const finalRes = await res.json();
+      this.listItems = finalRes;
+      if (finalRes[0].totalCount) {
+        this.pageCounter = finalRes[0].totalCount
+      }
+    },
   },
   mounted() {
     this.getItems()
